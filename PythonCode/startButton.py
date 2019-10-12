8fccc59b810d93708cb3030b5c301c44c13b71ed
Somkieat Wongseansuk <tingtongkub5@hotmail.com>
2019-10-12 17:10:33 +0700

    Add HR naja

--- PythonCode/startButton.py
+++ PythonCode/startButton.py
@@ -14,11 +14,11 @@ import json
 import librosa
 import datetime
 import csv
-
-
-exitFlag = 0
-finish_record = False
-count = 0
+import fitbit
+from fitbit import gather_keys_oauth2 as Oauth2
+import pandas as pd 
+import datetime
+import time
 
 class myThread(threading.Thread):
     def __init__(self, threadID, name, counter):
@@ -188,6 +188,39 @@ def add_to_column_in_last_row(workbook, workbook_name, username, amount_of_word,
         c = csv.writer(f)
         for r in sh.rows:
             c.writerow([cell.value for cell in r])
+
+def heart_rate_detection():
+    yesterday = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"))
+    yesterday2 = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"))
+    today = str(datetime.datetime.now().strftime("%Y-%m-%d"))
+    y = datetime.datetime.now()
+    timeEnd = y.strftime("%H"":""%M")
+    timeStart = y - datetime.timedelta(seconds=1000)
+    timeStart = timeStart.strftime("%H"":""%M")
+
+    fit_statsHR = auth2_client.intraday_time_series('activities/heart', base_date=today, detail_level='1sec' , start_time=timeStart, end_time = timeEnd )
+    time_list = []
+    val_list = []
+    for i in fit_statsHR['activities-heart-intraday']['dataset']:
+        val_list.append(i['value'])
+        time_list.append(i['time'])
+    heartdf = pd.DataFrame({'Heart Rate':val_list,'Time':time_list})
+    print(heartdf)
+
+
+CLIENT_ID = '22B63K'
+CLIENT_SECRET = '40c8ec5af315783465ef0b72e071d538'
+
+server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
+server.browser_authorize()
+ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
+
+
+exitFlag = 0
+finish_record = False
+count = 0
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
